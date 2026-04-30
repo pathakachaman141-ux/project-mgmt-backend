38ffba48204a3a04c1adf8f4125e107f560f3698
pathakachaman141-ux <pathakachaman141@gmail.com>
2026-05-01 05:02:15 +0530

    Update UserController.java

--- src/main/java/com/projectManagementApp/controllers/UserController.java
+++ src/main/java/com/projectManagementApp/controllers/UserController.java
@@ -176,6 +176,6 @@ public class UserController {
 
 	@GetMapping("/hello")
 	public String home(){
-		return "Hello World, Server launched successfully"
+		return "Hello World, Server launched successfully";
 	}
 }
